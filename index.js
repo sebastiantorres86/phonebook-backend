3c499009fb66894c193067db62f9a8761f832e45
Sebastian Torres <seba.torres.dev@gmail.com>
2021-09-22 12:43:17 -0300

    Step4 (#4)
    * step1
    
    * step2
    
    * step3
    
    * step4

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
