bd3c85ab6b8bde4bc91504165bd565dcc360f22f
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-09-29 10:40:49 -0300

    phonebook database, step7

--- models/person.js
+++ models/person.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-mongoose.connect(url)
+mongoose
+  .connect(url)
   .then(result => {
     console.log('connected to MongoDB')
   })
-  .catch((error) => {
+  .catch(error => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
-  })
+const personSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  number: { type: String, required: true, unique: true }
+})
 
-  personSchema.set('toJSON', {
+personSchema.plugin(uniqueValidator)
+
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
