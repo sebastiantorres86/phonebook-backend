8a367809332c6db2aa1fefcfbe05873cbc05f2a7
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-09-29 18:41:32 -0300

    hotfix

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
